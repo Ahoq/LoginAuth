acb3cc8d2e7eaa5832ac73108a7d117a60c3b8eb
Adnan Hoq <adnanbinhoq@gmail.com>
2017-02-18 13:51:27 -0500

    Update urls.py

--- logauth/urls.py
+++ logauth/urls.py
@@ -1,9 +1,6 @@
-#!python
-# log/urls.py
 from django.conf.urls import url
 from . import views
 
-# We are adding a URL called /home
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^register/$', views.register_page, name='register')
